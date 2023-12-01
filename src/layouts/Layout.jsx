e10bbff7aafece3a0fd69569a8ab4ab213c442db
ReyFzy <reyzafauzy7@gmail.com>
2023-12-01 08:30:26 +0700

    initial commit

--- src/layouts/Layout.jsx
+++ src/layouts/Layout.jsx
@@ -1,6 +1,6 @@
 const Layout = ({ children }) => {
     return (
-        <div className="flex flex-col items-center w-screen scroll-smooth -z-50">{children}</div>
+        <div className="flex flex-col items-center w-screen scroll-smooth overflow-x-hidden -z-50">{children}</div>
     )
 };
 export default Layout;
